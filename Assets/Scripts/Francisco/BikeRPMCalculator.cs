b4ac9d0c025d16fce4a59f80fbaa60ace1621afb
Francisco V. Diaz <fevarga1@uc.cl>
2025-07-08 16:05:15 -0400

    added tts and vfx to medipipe stationary bike (rpm calculator not working properly).

--- Assets/Scripts/Francisco/BikeRPMCalculator.cs
+++ Assets/Scripts/Francisco/BikeRPMCalculator.cs
@@ -1,8 +1,23 @@
 using System;
-    using UnityEngine;
+using Meta.WitAi.TTS.Utilities;
+using UnityEngine;
     
     public class BikeRPMCalculator : MonoBehaviour
     {
+        [SerializeField] private TTSSpeaker ttsSpeaker;
+        [SerializeField] private MotivationalMessages motivationalMessages;
+        [SerializeField] private ParticleSystem sequenceCompletedParticles;
+        [SerializeField] private int sequenceDuration = 20;
+        [SerializeField] private int rpmMinTarget = 100;
+        [SerializeField] private int rpmMaxTarget = 300;
+        [SerializeField] private string[] ttsOnSequenceRepetitionsCompleted = new string[]
+        {
+            "Great job! You completed the exercise!",
+            "Well done! You've finished the sequence!",
+            "Fantastic! You've completed the exercise sequence!",
+        };
+
+        [SerializeField] private TMPro.TMP_Text timeDurationDisplay;
         [SerializeField] private TMPro.TMP_Text displayRPMText;
         [SerializeField] private float movementThreshold = 0.03f;
         [SerializeField] private int smoothingFrames = 5;
@@ -15,14 +30,20 @@ using System;
     
         private float[] rightFootBuffer;
         private int bufferIndex;
+        private float timeSinceLastTTS;
+        private bool hasFinishedSequence;
+        
+        private float currentSequenceTime;
     
         private void Start()
         {
             rightFootBuffer = new float[smoothingFrames];
+            ttsSpeaker.Speak($"Let's start the cycling exercise. Keep pedaling for {sequenceDuration} seconds.");
         }
     
         void Update()
         {
+            if (hasFinishedSequence) return;
             if (!MediaPipeBodyController.Instance.MediaPipeTransformPointsParent) return;
             Vector3 rightFootPos =
                 MediaPipeBodyController.Instance.MediaPipeTransformPointsParent.GetChild(28).position;
@@ -34,7 +55,9 @@ using System;
             {
                 CheckFootRevolution(ref smoothedRightY, ref rightFootLastLowestY, ref rightFootRevolutions, ref rightFootGoingUp);
             }
-    
+            CountTime();
+            if (CheckSequenceTime()) return;
+            CheckCurrentSpeed();
             if (timer >= 1f)
             {
                 rpm = rightFootRevolutions * 60f;
@@ -43,7 +66,50 @@ using System;
                 timer = 0f;
             }
         }
-    
+
+        private void CountTime()
+        {
+            if (rpm >= rpmMinTarget && rpm <= rpmMaxTarget)
+            {
+                currentSequenceTime += Time.deltaTime;
+                // display time in 00:00 format
+                if (timeDurationDisplay)
+                {
+                    int minutes = Mathf.FloorToInt(currentSequenceTime / 60);
+                    int seconds = Mathf.FloorToInt(currentSequenceTime % 60);
+                    timeDurationDisplay.text = $"{minutes:00}:{seconds:00}";
+                }
+            }
+        }
+
+        private void CheckCurrentSpeed()
+        {
+            if (timeSinceLastTTS < 5f)
+            {
+                timeSinceLastTTS += Time.deltaTime;
+                return;
+            }
+            timeSinceLastTTS = 0f;
+            if (rpm > rpmMaxTarget) ttsSpeaker.Speak("Slow down!");
+            else if (rpm < rpmMinTarget) ttsSpeaker.Speak("Speed up!");
+        }
+
+        private bool CheckSequenceTime()
+        {
+            if (currentSequenceTime >= sequenceDuration)
+            {
+                hasFinishedSequence = true;
+                motivationalMessages.ShowMessage();
+                sequenceCompletedParticles.Play();
+                timeDurationDisplay.text = string.Empty;
+                displayRPMText.text = string.Empty;
+                ttsSpeaker.Speak(
+                    ttsOnSequenceRepetitionsCompleted[UnityEngine.Random.Range(0, ttsOnSequenceRepetitionsCompleted.Length)]);
+                return true;
+            }
+            return false;
+        }
+
         private float SmoothValue(float newValue, float[] buffer, ref int index)
         {
             buffer[index % smoothingFrames] = newValue;
